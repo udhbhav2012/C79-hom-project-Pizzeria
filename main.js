9ef5d926f5952ec5d613d48a83cc843437eecc71
udhbhav2012 <82303525+udhbhav2012@users.noreply.github.com>
2021-06-13 11:40:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+menu_list_items = ["Chicken Tandoori", "Veg Supreme Burger", "Paneer Tikka", "Chicken Chili", "Cheese Burst Pizza"];
+
+function run()
+{
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
+
+function add_new_menu_item()
+{
+    new_menu_item = document.getElementById("new_menu_item").value;
+    menu_list_items.push(new_menu_item);
+    menu_list_items.sort();
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
